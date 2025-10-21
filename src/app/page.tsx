9518d5b1abb82fbc7ed3d7998893da34a89f9de8
Rahul Chaudhari_ <rahulgc545@gmail.com>
2025-10-21 15:50:46 +0530

    db tst

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-export default function Page() {
+import prisma from "@/lib/db"
+
+export default async function Page() {
+
+  const users = await prisma.user.findMany();
+
   return (
     <div className="flex justify-center items-center h-screen" >
       HEllo world
+      {JSON.stringify(users)}
     </div>
   )
 }
